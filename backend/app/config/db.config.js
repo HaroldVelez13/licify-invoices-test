05593b980a05bcf373837d3609cb04959c6adfcd
Harold Velez <haroldvelez113@gmail.com>
2023-05-02 14:47:48 -0500

    fix: Globals env backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  DB_URL: process.env.MONGODB_URI,
-  ORIGIN_URL: process.env.ORIGIN_URI ?? "http://localhost:8081",
+  DB_URL:
+    process.env.MONGODB_URI ??
+    "mongodb://mongo:41tUDGCyeRaOQHxBlJvp@containers-us-west-113.railway.app:7822/test",
+  ORIGIN_URL:
+    process.env.ORIGIN_URI ?? "https://licify-invoices-test.vercel.app",
   APP_PORT: process.env.APP_PORT ?? 3000,
 };
